3026ab5aa2f2fb8a48fe38db9481b4de3fe8520d
Rosalie Newman <rosalienewman0@gmail.com>
2022-09-22 20:38:21 -0400

    categories:id route works and returns a list of all the bookmarks with a specified id as per URL

--- index.js
+++ index.js
@@ -7,6 +7,50 @@ app.use(staticMiddleware);
 
 const { db, Bookmark, Category } = require('./server');
 
+app.get('/categories/:id', async (req, res, next) => {
+	const id = +req.params.id;
+	// const categoryObject = await Category.findByPk(id);
+
+	const bookmarks = await Bookmark.findAll();
+
+	const filteredBookmarks = bookmarks.filter(
+		(bookmark) => bookmark.dataValues.categoryId === id
+	);
+	console.log(filteredBookmarks);
+	//res.send(filteredBookmarks);
+
+	res.send(
+		`
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="stylesheet" href="style.css" />
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Bookmarks of a certain category</title>
+	</head>
+	<body>
+	<h1> Bookmarks of category ${id}
+		<ul>
+			${filteredBookmarks
+				.map(
+					(bookmark) =>
+						`
+			<h1>${bookmark.name}</h1>
+			<li>primary key id === ${bookmark.id}</li>
+			<li>${bookmark.url}</li>
+			<li>categoryId === ${bookmark.categoryId}</li>
+			`
+				)
+				.join('')}
+		</ul>
+	</body>
+</html>
+`
+	);
+});
+
 app.get('/', async (req, res, next) => {
 	//const allCategories = await Category.findAll();
 	const bookmarks = await Bookmark.findAll();
@@ -30,9 +74,9 @@ app.get('/', async (req, res, next) => {
 					(bookmark) =>
 						`
         <h1>${bookmark.name}</h1>
-        <li>${bookmark.id}</li>
+        <li>primary key id === ${bookmark.id}</li>
         <li>${bookmark.url}</li>
-        <li>${bookmark.categoryId}</li>
+        <li>categoryId === ${bookmark.categoryId}</li>
         `
 				)
 				.join('')}
